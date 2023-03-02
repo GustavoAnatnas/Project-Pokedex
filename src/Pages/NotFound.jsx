3ba3f1d090f0543b62fea47fab7b7b1a3550b083
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2023-03-02 15:48:53 -0300

    Cria elemento NotFound para páginas não existentes

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import gifPikachu from '../Pikachu.gif';
 
 function NotFound() {
   return (
-    <div>Página não Encontrada!</div>
+    <body className="bg-gray-100 h-screen text-black text-4xl flex flex-col justify-center items-center
+"
+    >
+      <img src={gifPikachu} alt="Pikachu acenando" />
+      <br />
+      <h1>404</h1>
+      <h1>Página não encontrada!</h1>
+    </body>
   );
 }
 
